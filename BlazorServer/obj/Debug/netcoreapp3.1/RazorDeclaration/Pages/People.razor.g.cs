3f51207f55960ddeef73701053e8156a6731f5f4
Bas <onrust.bas@gmail.com>
2021-02-24 13:09:43 +0100

    Changed folder location

--- BlazorServer/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/People.razor.g.cs
+++ BlazorServer/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/People.razor.g.cs
@@ -13,84 +13,84 @@ namespace BlazorServer.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Bas\source\repos\ConnectingToMySQL\BlazorServer\_Imports.razor"
+#line 1 "C:\Users\Bas\Documents\GitHub\ConnectingToMySQL\BlazorServer\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Bas\source\repos\ConnectingToMySQL\BlazorServer\_Imports.razor"
+#line 2 "C:\Users\Bas\Documents\GitHub\ConnectingToMySQL\BlazorServer\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Bas\source\repos\ConnectingToMySQL\BlazorServer\_Imports.razor"
+#line 3 "C:\Users\Bas\Documents\GitHub\ConnectingToMySQL\BlazorServer\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Bas\source\repos\ConnectingToMySQL\BlazorServer\_Imports.razor"
+#line 4 "C:\Users\Bas\Documents\GitHub\ConnectingToMySQL\BlazorServer\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Bas\source\repos\ConnectingToMySQL\BlazorServer\_Imports.razor"
+#line 5 "C:\Users\Bas\Documents\GitHub\ConnectingToMySQL\BlazorServer\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Bas\source\repos\ConnectingToMySQL\BlazorServer\_Imports.razor"
+#line 6 "C:\Users\Bas\Documents\GitHub\ConnectingToMySQL\BlazorServer\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Bas\source\repos\ConnectingToMySQL\BlazorServer\_Imports.razor"
+#line 7 "C:\Users\Bas\Documents\GitHub\ConnectingToMySQL\BlazorServer\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Bas\source\repos\ConnectingToMySQL\BlazorServer\_Imports.razor"
+#line 8 "C:\Users\Bas\Documents\GitHub\ConnectingToMySQL\BlazorServer\_Imports.razor"
 using BlazorServer;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Bas\source\repos\ConnectingToMySQL\BlazorServer\_Imports.razor"
+#line 9 "C:\Users\Bas\Documents\GitHub\ConnectingToMySQL\BlazorServer\_Imports.razor"
 using BlazorServer.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Bas\source\repos\ConnectingToMySQL\BlazorServer\Pages\People.razor"
+#line 3 "C:\Users\Bas\Documents\GitHub\ConnectingToMySQL\BlazorServer\Pages\People.razor"
 using DataLibrary;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Bas\source\repos\ConnectingToMySQL\BlazorServer\Pages\People.razor"
+#line 4 "C:\Users\Bas\Documents\GitHub\ConnectingToMySQL\BlazorServer\Pages\People.razor"
 using BlazorServer.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Bas\source\repos\ConnectingToMySQL\BlazorServer\Pages\People.razor"
+#line 5 "C:\Users\Bas\Documents\GitHub\ConnectingToMySQL\BlazorServer\Pages\People.razor"
 using Microsoft.Extensions.Configuration;
 
 #line default
@@ -105,7 +105,7 @@ using Microsoft.Extensions.Configuration;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 29 "C:\Users\Bas\source\repos\ConnectingToMySQL\BlazorServer\Pages\People.razor"
+#line 29 "C:\Users\Bas\Documents\GitHub\ConnectingToMySQL\BlazorServer\Pages\People.razor"
        
     List<PersonModel> people;
 
